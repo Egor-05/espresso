7079fffb4414c5f171990bedae5c95a2746e8536
egor <e.kulpin@mail.ru>
2020-11-30 17:22:13 +0300

    commit

---
+++ editer.py
@@ -0,0 +1,75 @@
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QPushButton, QTableWidget, QTableWidgetItem, QDialog, QComboBox, QStatusBar
+from PyQt5.Qt import *
+from PyQt5 import QtCore, QtGui
+import sqlite3
+from addEditCoffeeForm import Ui_Dialog
+import sys
+
+
+class SecondWindow(QDialog, Ui_Dialog):
+    def __init__(self, root, **kwargs):
+        super().__init__(root, **kwargs)
+        self.setupUi(self)
+        self.main = root
+        self.build()
+
+
+    def build(self):
+        self.pushButton.clicked.connect(self.clos)
+        if self.main.set:
+            self.lineEdit.setText(self.main.tableWidget.item(self.main.tableWidget.currentRow(), 1).text())
+            self.lineEdit_2.setText(self.main.tableWidget.item(self.main.tableWidget.currentRow(), 2).text())
+            self.lineEdit_8.setText(self.main.tableWidget.item(self.main.tableWidget.currentRow(), 3).text())
+            self.lineEdit_7.setText(self.main.tableWidget.item(self.main.tableWidget.currentRow(), 5).text())
+            self.lineEdit_4.setText(self.main.tableWidget.item(self.main.tableWidget.currentRow(), 6).text())
+            self.lineEdit_5.setText(self.main.tableWidget.item(self.main.tableWidget.currentRow(), 7).text())
+
+    def clos(self):
+        try:
+            if not self.main.set:
+                if self.lineEdit_4.text().isdigit() and self.lineEdit_5.text().isdigit():
+                    with sqlite3.connect('coffee.sqlite') as con:
+                        cur = con.cursor()
+                        cur.execute("""INSERT INTO coffee (
+                                                          name,
+                                                          grade,
+                                                          roast,
+                                                          type,
+                                                          taste,
+                                                          price,
+                                                          size
+                                                          )
+                                                          VALUES (?,?,?,?,?,?,?)""", [self.lineEdit.text(),
+                                                                                      self.lineEdit_2.text(),
+                                                                                      self.lineEdit_8.text(),
+                                                                                      self.comboBox.currentText(),
+                                                                                      self.lineEdit_7.text(),
+                                                                                      self.lineEdit_4.text(),
+                                                                                      self.lineEdit_5.text()])
+                        con.commit()
+                    self.main.fill_table()
+
+                    self.main.tableWidget.setCurrentCell(self.main.tableWidget.rowCount() - 1, 0)
+                    self.close()
+                else:
+                    valid = QMessageBox.question(self.pushButton, '',
+                                                 'Заполните все значения',
+                                                 QMessageBox.Ok)
+            else:
+                for j, elem in enumerate([self.lineEdit.text(),
+                                          self.lineEdit_2.text(),
+                                          self.lineEdit_8.text(),
+                                          self.comboBox.currentText(),
+                                          self.lineEdit_7.text(),
+                                          self.lineEdit_4.text(),
+                                          self.lineEdit_5.text()]):
+                    self.main.tableWidget.setItem(
+                        self.main.tableWidget.currentRow(), j + 1, QTableWidgetItem(str(elem)))
+                    self.main.fill_bd()
+                self.close()
+
+        except ValueError:
+            valid = QMessageBox.question(self.pushButton, '',
+                                         'Неверные значения в числовых полях',
+                                         QMessageBox.Ok)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from ui import Ui_Dialog
+from window import Ui_Dialog
 import sqlite3
 import sys
 from PyQt5 import QtWidgets
+from editer import SecondWindow
 
 
 class MyWidget(QtWidgets.QMainWindow, Ui_Dialog):
@@ -9,9 +10,19 @@ class MyWidget(QtWidgets.QMainWindow, Ui_Dialog):
         super().__init__()
         self.setupUi(self)
         self.fill_table()
+        self.btn1.clicked.connect(self.openWin)
+        self.btn.clicked.connect(self.set_coffee)
+        self.secondWin = None
+        self.set = False
 
-    def add_elem(self):
+    def set_coffee(self):
+        self.set = True
         self.openWin()
+
+    def openWin(self):
+        if not self.secondWin:
+            self.secondWin = SecondWindow(self)
+        self.secondWin.show()
 
     def fill_table(self):
         with sqlite3.connect('coffee.sqlite') as con:
@@ -22,6 +33,42 @@ class MyWidget(QtWidgets.QMainWindow, Ui_Dialog):
                 for j, elem in enumerate(row):
                     self.tableWidget.setItem(
                         i, j, QtWidgets.QTableWidgetItem(str(elem)))
+
+    def change_films(self):
+        if self.tableWidget.currentRow() < 0:
+            valid = QtWidgets.QMessageBox.question(self.set_film, '',
+                                                   'Вы забыли выделить поле в таблице',
+                                                   QtWidgets.QMessageBox.Ok)
+            return
+        self.set = True
+        self.openWin()
+
+    def fill_bd(self):
+        with sqlite3.connect('coffee.sqlite') as con:
+            rows = self.tableWidget.rowCount()
+            cols = self.tableWidget.columnCount()
+            data = []
+            cur = con.cursor()
+            for row in range(rows):
+                tmp = []
+                for col in range(1, cols):
+                    tmp.append(self.tableWidget.item(row, col).text())
+                data.append(tmp)
+
+            cur.execute("DELETE FROM coffee")
+            con.commit()
+            for i in data:
+                cur.execute("""INSERT INTO coffee (
+                                                      name,
+                                                      grade,
+                                                      roast,
+                                                      type,
+                                                      taste,
+                                                      price,
+                                                      size
+                                                  )
+                                              VALUES (?,?,?,?,?,?,?)""", i)
+            con.commit()
 
 
 if __name__ == '__main__':
